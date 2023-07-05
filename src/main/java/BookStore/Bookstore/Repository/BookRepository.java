76e917f3df064155fbc367a6c64c36c65876dbd7
sitinajwa00 <s.najwapandi@gmail.com>
2023-07-05 22:29:14 +0800

    Update Book

--- src/main/java/BookStore/Bookstore/Repository/BookRepository.java
+++ src/main/java/BookStore/Bookstore/Repository/BookRepository.java
@@ -1,9 +1,11 @@
 package BookStore.Bookstore.Repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import BookStore.Bookstore.Model.Book;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
 
 }
